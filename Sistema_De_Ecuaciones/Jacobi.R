2a216ad222168b6d9a2bf85e1f616eca30591211
Yorbre25 <62673860+Yorbre25@users.noreply.github.com>
2022-04-29 15:07:06 -0600

    Initial Commit

---
+++ Sistema_De_Ecuaciones/Jacobi.R
@@ -0,0 +1,33 @@
+
+jacobi <- function(matriz_a, b, x0, tol, itermax) {
+    d <- diag(matriz_a)
+    matriz_d <- diag(d)
+    d_inv <- 1 / d
+    matriz_d_inv <- diag(d_inv)
+    l_mas_u <- matriz_a - matriz_d
+    z <- matriz_d_inv %*%  b
+    m <-  -matriz_d_inv %*% l_mas_u
+    xk <- x0
+
+    for (i in seq(1, itermax, by = 1)) {
+        xk <- m %*% xk + z
+        error <- norm(matriz_a %*% xk - b, type = "2")
+        if(error < tol) {
+            break
+        }
+    }
+    print(xk)
+    print(i)
+    print(error)
+}
+
+x0 <- c(0, 0, 0)
+r1 <- c(5, 1, 0)
+r2 <- c(1, 5, 1)
+r3 <- c(0, 1, 5)
+matriz_a <- rbind(r1, rbind(r2, r3))
+b <- c(6, 7, 6)
+tol <- 10^-10
+itermax <- 1000
+
+jacobi(matriz_a, b, x0, tol, itermax)
---
+++ Sistema_De_Ecuaciones/Thomas.R
@@ -0,0 +1,54 @@
+
+thomas <- function(mat_A, d){ # nolint
+    m <- nrow(mat_A)
+    a_vec <- diag(mat_A, 1)
+    b_vec <- diag(mat_A)
+    c_vec <- diag(mat_A, -1)
+
+    x <- c()
+    p <- c()
+    q <- c()
+
+    #valores iniciales de p y q
+    p[1] <- c_vec[1] / b_vec[1]
+    q[1] <- d[1] / b_vec[1]
+
+    for (i in seq(2, m - 1, by = -1)) {
+        denom <- b_vec[i] - p[i - 1] * a_vec[i - 1]
+        #Calculo de p
+        p[i] <- c_vec[i] / denom
+        #Calculo de q (menos el valor final)
+        num <- d[i] - q[i - 1] * a_vec[i - 1]
+        q[i] <- num / denom
+    }
+    #Calculo de valor final de q
+    num <- d[m] - q[m - 1] * a_vec[m - 1]
+    denom <- b_vec[m] - p[m - 1] * a_vec[m - 1]
+    q[m] <- num / denom
+
+    print(p)
+    print(q)
+
+    # #Valor final de X
+    # x[m] <- q[m]
+    # #El resto de valores de X
+    # for (j in seq(m - 1, 0, by = -1)) {
+    #     x[j] <- q[j] - p[j] * x[j + 1]
+    # }
+    # print(x)
+}
+
+get_diagonal <- function(mat, offset) {
+    m <- nrow(mat)
+    n <- ncol(mat)
+    
+    }
+
+r1 <- c(5, 1, 0, 0)
+r2 <- c(1, 5, 1, 0)
+r3 <- c(0, 1, 5, 1)
+r4 <- c(0, 0, 1, 5)
+mat_A <- rbind(r1, rbind(r2, rbind(r3, r4))) #nolint
+d <- c(-12, -14, -14, -12)
+# thomas(mat_A, d)
+

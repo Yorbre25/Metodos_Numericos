15bf24cb3bae628c086bd6f6da6ee1aa0314dfce
Yorbre25 <62673860+Yorbre25@users.noreply.github.com>
2022-05-01 11:03:29 -0600

    add: Thomas completada

--- Sistema_De_Ecuaciones/Thomas.R
+++ Sistema_De_Ecuaciones/Thomas.R
@@ -1,19 +1,21 @@
 
-thomas <- function(mat_A, d){ # nolint
-    m <- nrow(mat_A)
-    a_vec <- diag(mat_A, 1)
-    b_vec <- diag(mat_A)
-    c_vec <- diag(mat_A, -1)
+thomas <- function(mat, d){ # nolint
+    m <- nrow(mat)
+    cba <- get_tridiagonal(mat)
 
-    x <- c()
-    p <- c()
-    q <- c()
+    c_vec <- cba[[1]]
+    b_vec <- cba[[2]]
+    a_vec <- cba[[3]]
+
+    x <- rep(0, m)
+    p <- rep(0, m)
+    q <- rep(0, m)
 
     #valores iniciales de p y q
     p[1] <- c_vec[1] / b_vec[1]
     q[1] <- d[1] / b_vec[1]
 
-    for (i in seq(2, m - 1, by = -1)) {
+    for (i in seq(2, m - 1, by = 1)) {
         denom <- b_vec[i] - p[i - 1] * a_vec[i - 1]
         #Calculo de p
         p[i] <- c_vec[i] / denom
@@ -30,33 +32,42 @@ thomas <- function(mat_A, d){ # nolint
     print(q)
 
     # #Valor final de X
-    # x[m] <- q[m]
-    # #El resto de valores de X
-    # for (j in seq(m - 1, 0, by = -1)) {
-    #     x[j] <- q[j] - p[j] * x[j + 1]
-    # }
-    # print(x)
+    x[m] <- q[m]
+    #El resto de valores de X
+    for (j in seq(m - 1, 0, by = -1)) {
+        x[j] <- q[j] - p[j] * x[j + 1]
+    }
+    print(x)
 }
 
-get_diagonal <- function(mat, offset) {
+get_tridiagonal <- function(mat) {
     m <- nrow(mat)
-    # n <- ncol(mat)
-    d <- c()
-    # for (i in seq(1, m, by = 1)) {
-    #     d[i] <- mat[i, i + offset]
-    # }
+
+    c_vec <- c()
+    b_vec <- diag(mat)
+    a_vec <- c()
+
+    for (i in seq(1, m - 1, by = 1)) {
+        c_vec[i] <- mat[i, i + 1]
+        a_vec[i] <- mat[i + 1, i]
+    }
     for (i in seq(1, m - 1, by = 1)) {
         d[i] <- mat[i, i]
     }
-    print(d)
+
+    cba <- list(c_vec, b_vec, a_vec)
+    return(cba)
     }
 
-r1 <- c(5, 1, 0, 0) 
-r2 <- c(1, 5, 1, 0)
-r3 <- c(0, 1, 5, 1)
-r4 <- c(0, 0, 1, 5)
-mat_A <- rbind(r1, rbind(r2, rbind(r3, r4))) #nolint
-d <- c(-12, -14, -14, -12)
-get_diagonal(mat_A, -1)
-# thomas(mat_A, d)
+# r1 <- c(5, 1, 0, 0)
+# r2 <- c(1, 5, 1, 0)
+# r3 <- c(0, 1, 5, 1)
+# r4 <- c(0, 0, 1, 5)
 
+r1 <- c(1, 5, 0)
+r2 <- c(6, 2, 7)
+r3 <- c(0, 8, 3)
+# mat <- rbind(r1, rbind(r2, rbind(r3, r4))) #nolint
+mat <- rbind(r1, rbind(r2, r3)) #nolint
+d <- c(6, 7, 6)
+thomas(mat, d)
